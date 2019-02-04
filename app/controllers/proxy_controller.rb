ebe39acbe419b6ae8d4473364dc930522a48aad9
Ryan Zuwala <ryanzuwala@gmail.com>
2019-02-03 23:06:24 -0500

    wip - implement http get requests

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -3,12 +3,20 @@ require 'net/http'
 class ProxyController < ActionController::Base
     # GET requests
     def show
-        url = params[:url]
+        url_param = params[:url]
+        url_param.gsub!('http:/', 'http://')
+        url_param.gsub!('https:/', 'https://')
 
         #response = Net::HTTP.get(URI.parse(url))
+        url = URI.parse(url_param)
+        #url = URI.parse("http://www.google.com/")
+        req = Net::HTTP::Get.new(url.to_s)
+        res = Net::HTTP.start(url.host, url.port) { |http|
+            http.request(req)
+        }
 
         respond_to do |format|
-            format.html { render html: url }
+            format.html { render html: res.body.html_safe }
         end
     end
 

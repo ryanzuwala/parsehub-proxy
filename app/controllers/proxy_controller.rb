4b654aeb681498ca36362a642e68acff0976349f
Ryan Zuwala <ryanzuwala@gmail.com>
2019-02-04 21:46:59 -0500

    fix http requests, start fixing https

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -3,24 +3,35 @@ require 'net/http'
 class ProxyController < ActionController::Base
     # GET requests
     def show
-        url_param = params[:url]
-        url_param.gsub!('http:/', 'http://')
-        url_param.gsub!('https:/', 'https://')
-
-        #response = Net::HTTP.get(URI.parse(url))
-        url = URI.parse(url_param)
-        #url = URI.parse("http://www.google.com/")
-        req = Net::HTTP::Get.new(url.to_s)
-        res = Net::HTTP.start(url.host, url.port) { |http|
-            http.request(req)
-        }
+        response = build_http_client :get, params[:url]
 
         respond_to do |format|
-            format.html { render html: res.body.html_safe }
+            format.html { render html: response }
         end
     end
 
     # POST requests
     def create
     end
+
+    private
+    def build_http_client(method, url)
+        url_param = params[:url]
+
+        url_param.gsub!('http:/', 'http://')
+        url_param.gsub!('https:/', 'https://')
+        url_param.prepend "http://" unless url_param.start_with? /https?\:\/\//
+
+        url = URI.parse(url_param)
+        
+        req = Net::HTTP::Get.new(url)
+        req["User-Agent"] = request.headers["User-Agent"]
+
+        res = Net::HTTP.start(url.host, url.port) { |http|
+            http.request(req)
+        }
+
+        res.body.html_safe
+    end
+
 end

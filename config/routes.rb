41d3500389e40f00ab4db5fb642cb9fed1e81dc4
Ryan Zuwala <ryanzuwala@gmail.com>
2019-02-04 22:18:40 -0500

    fix post route, clean up code and add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :proxy, only: [:create]
+  #resources :proxy, only: [:create]
   get '/proxy/*url', to: "proxy#show", :constraints => { url: /[\\\/\:\-_a-zA-Z0-9\.]+/ }
+  post '/proxy/*url', to: "proxy#create", :constraints => { url: /[\\\/\:\-_a-zA-Z0-9\.]+/ }
 end
